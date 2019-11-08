d50733678c108d594cf6a89ac4400e914771b48c
Mohammed FAKHAM <mohammed.fakham@gmail.com>
2019-11-08 14:31:29 +0100

    chore: fix footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,14 +1,11 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 //use this blank tempate to create new component
 //import styles from './DashedBox.css';
 
 class Footer extends Component {
-
-  state = {}
-  componentDidMount() {
-
-  }
+  state = {};
+  componentDidMount() {}
   render() {
     var FullYear = new Date().getFullYear();
     return (
@@ -17,32 +14,28 @@ class Footer extends Component {
           <nav className="float-left">
             <ul>
               <li>
-                <a href="">
-                  Gnotes
-            </a>
+                <a href="">Gnotes</a>
               </li>
               <li>
-                <a href="">
-                  About Us
-            </a>
+                <a href="">About Us</a>
               </li>
               <li>
-                <a href="">
-                  Blog
-            </a>
+                <a href="">Blog</a>
               </li>
               <li>
-                <a href="">
-                  Licenses
-            </a>
+                <a href="">Licenses</a>
               </li>
             </ul>
           </nav>
-          <div className="copyright float-right"> &copy;
-            {FullYear}
-            , made with <i className="material-icons">favorite</i> by
-        <a href="" target="_blank">BDIO</a> for a better ENSAM.
-      </div>
+          <div className="copyright float-right">
+            {" "}
+            &copy;
+            {FullYear}, made with <i className="material-icons">favorite</i> by{" "}
+            <a href="" target="_blank">
+              BDIO
+            </a>{" "}
+            for a better ENSAM.
+          </div>
         </div>
       </footer>
     );
